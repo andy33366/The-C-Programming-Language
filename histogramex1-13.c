6a7c1938c2b982aaa2292b4fe67420537b33f607
andy33366 <andy704389@gmail.com>
2023-08-27 18:34:10 -0500

    added histogram ex-13

---
+++ histogramex1-13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define IN 1 /*inside a word*/
+#define OUT 0 /*outside a word*/
+
+int main () {
+	int c;
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\n' || c == '\t'){
+			printf("\n");
+		}
+		printf("_");
+	}
+}
