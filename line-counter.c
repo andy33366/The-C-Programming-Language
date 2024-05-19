0235d10c138eb76dbf6e6aa87b453242297394f8
andy33366 <andy704389@gmail.com>
2024-05-19 12:32:20 -0500

    adding space counter and tab counter to line-counter

--- line-counter.c
+++ line-counter.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
 int main() {
-	int c, nl;
+	int c, nl, blanks, tabs;
 
 	nl = 0;
+	blanks = 0;
+	tabs = 0;
+
 	while ((c = getchar()) != EOF)
 		if (c == '\n')
 			++nl;
+		if (c == ' ')
+			++blanks;
+		if (c == '\t')
+			++tabs;
+
+	printf("newlines:");		
 	printf("%d\n", nl);
+	printf("\nspaces:");
+	printf("%d\n", blanks);
+	printf("\ntabs:");
+	printf("%d\n", tabs);
 	
 
 }
