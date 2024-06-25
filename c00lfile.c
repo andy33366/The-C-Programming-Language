b5dad788a09d98c08497acb09197b17585638f25
andy33366 <andy704389@gmail.com>
2024-06-25 08:29:33 -0500

    changed c00lout to print a txt message

--- c00lfile.c
+++ c00lfile.c
@@ -1,12 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(int argc, char *argv[]) {
+	
+	FILE *fptr;
+
+	char c;
+
 	if(argc==2) {
-		printf("Hello, %s!\n", argv[1]);
+
+		//open file
+		fptr = fopen(argv[1], "r");
+		if (fptr == NULL){
+			printf("cannot open file");
+			exit(0);
+		}
+
+		//read contents from file
+		c = fgetc(fptr);
+		while (c != EOF){
+
+			printf("%c", c);
+			c = fgetc(fptr);
+
+		}
+		
+		fclose(fptr);
+		return 0;
+		
+		//printf("Hello, %s!\n", argv[1]);
+		
+
 	}
+
 	else{
-		fprintf(stderr, "Usage: %s <name>\n", argv[0]);
+
+		fprintf(stderr, "Usage: c00lout nameoffile", argv[0]);
 		return 1;
+
 	}
+
 	return 0;
 }
